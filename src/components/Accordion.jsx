295f7426a4e165a09148358117c56576f9859f31
ziada <mohammed567-tech>
2024-06-06 09:30:43 +0300

    updates

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,12 +1,49 @@
+import { useEffect, useState } from "react";
 import ItemAccordion from "./ItemAccordion";
-import { faqs } from "./dbFaqs";
 
 export default function Accordion() {
+  const [quotes, setQuotes] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchQuotes = async () => {
+    const url = "https://type.fit/api/quotes";
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      setQuotes(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="accordion">
-      {faqs.map((el, i) => (
-        <ItemAccordion num={i} title={el.title} description={el.text} />
-      ))}
+      {quotes.length > 0 ? (
+        quotes
+          .slice(0, 10)
+          .map((quote, index) => (
+            <ItemAccordion
+              key={index}
+              num={index}
+              title={quote.author ? quote.author : "Unknown Author"}
+              description={quote.text}
+            />
+          ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
--- src/components/ItemAccordion.jsx
+++ src/components/ItemAccordion.jsx
@@ -7,7 +7,7 @@ export default function ItemAccordion({ num, title, description }) {
   };
   return (
     <div className={`item ${isActive ? "open" : ""}`} onClick={handleToggle}>
-      <span className="number">{num > 9 ? num + 1 : `0${num + 1}`}</span>
+      <span className="number">{num < 9 ? `0${num + 1}` : num + 1}</span>
       <p className="text">{title}</p>
       <p className="icon">{isActive ? "-" : "+"}</p>
       {isActive ? <div className="content-box">{description}</div> : ""}
